57c38f83b0c3f25e7876e82aee6bec71c4f00fc9
Yago Pereira <yagopazvd@gmail.com>
2019-07-19 19:57:34 -0300

    Updates

---
+++ precache-manifest.38777acca5a50d1b3bae6c70d549806c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c26a81f6440479a4f8012a96fc06297",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "0904dcbe9abf92df525d",
+    "url": "/todo-app/static/css/main.b8911de2.chunk.css"
+  },
+  {
+    "revision": "88bb9b7ec85b8a957a06",
+    "url": "/todo-app/static/js/2.01f8cbe7.chunk.js"
+  },
+  {
+    "revision": "0904dcbe9abf92df525d",
+    "url": "/todo-app/static/js/main.382aad47.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
